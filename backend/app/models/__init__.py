d3614072f03ad5c057af745f51965549bebf687d
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-05-20 21:55:29 +0100

    feat(models): initialize models module and import necessary model classes

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models.candidat import Candidat
+from app.models.candidat_skill import CandidatSkills
+from app.models.department import Department
+from app.models.department_skill import DepartmentSkills
+from app.models.experience import Experience
+from app.models.resume import Resume
+from app.models.user import User
+
