31b5780e0a82c3ad842faf7e757854ad2226f821
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-05-21 11:13:58 +0100

    feat(route): add upload resume route and ensure the file validation

--- backend/app/api/routes/resume.py
+++ backend/app/api/routes/resume.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, File, UploadFile, status
 from sqlalchemy.orm import Session
 
 from app.core.database import get_db
-from app.schemas.resume import ResumeCreate, ResumeRead
+from app.schemas.resume import ResumeRead
 from app.services.resume_service import create_resume as create_resume_service
 
 
@@ -10,16 +10,10 @@ router = APIRouter(prefix="/resumes", tags=["resumes"])
 
 
 @router.post("/", response_model=ResumeRead, status_code=status.HTTP_201_CREATED)
-def create_resume(payload: ResumeCreate, db: Session = Depends(get_db)):
-	return create_resume_service(payload, db)
+def create_resume(file: UploadFile = File(...), db: Session = Depends(get_db)):
+	return create_resume_service(file, db)
 
 
-from app.core.database import engine
-from sqlalchemy import text
-@router.get("/test-db")
-def test_db():
-
-    with engine.connect() as connection:
-        connection.execute(text("SELECT 1"))
-
-    return {"message": "Database connected"}
+@router.post("/upload", response_model=ResumeRead, status_code=status.HTTP_201_CREATED)
+def upload_resume(file: UploadFile = File(...), db: Session = Depends(get_db)):
+	return create_resume_service(file, db)
